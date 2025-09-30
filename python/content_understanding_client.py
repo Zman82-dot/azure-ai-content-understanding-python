bde2a8c8d2fc4a732877f1be6357bf3e77335f41
Chien Yuan Chang <ds.chienyuanchang@gmail.com>
2025-09-30 14:44:13 -0700

    Improve Segmentation Description and Adjust Timeout (#104)

--- python/content_understanding_client.py
+++ python/content_understanding_client.py
@@ -20,7 +20,7 @@ from azure.storage.blob import (
 from azure.storage.blob.aio import ContainerClient
 
 
-POLL_TIMEOUT_SECONDS = 120
+POLL_TIMEOUT_SECONDS = 180
 
 
 @dataclass
--- tools/test_notebooks/test_notebooks.py
+++ tools/test_notebooks/test_notebooks.py
@@ -98,9 +98,9 @@ def run_all_notebooks(
 if __name__ == "__main__":
     args: List[str] = sys.argv[1:]
 
-    # NOTE: Define skip list (can use full paths or substrings)
+    # NOTE: Define skip list (can use full paths or substrings, e.g., "notebook_name.ipynb")
     skip_list = [
-        "build_person_directory.ipynb",  # Skip due to "new_face_image_path" needed to be added manually
+        "build_person_directory.ipynb",  # Skip due to face API limit wasn't lifted
     ]
 
     if not args:
